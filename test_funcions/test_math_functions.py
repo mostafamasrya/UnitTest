c688ae8d4b9fbd6c2153a75fbea1f2b0c5a06904
mostafamasrya <106432717+mostafamasrya@users.noreply.github.com>
2023-05-05 18:56:58 +0300

    second video

--- test_funcions/test_math_functions.py
+++ test_funcions/test_math_functions.py
@@ -1,12 +1,29 @@
 import math_functions
-
+import pytest
+import sys
+@pytest.mark.skipif(sys.version_info < (3,3),reason="skipped")
 def test_add():
     assert math_functions.add(7,3) == 10
     assert math_functions.add(7) == 9
     assert math_functions.add(5) == 7
     assert math_functions.add(-3,3) == 0
+    print("======-================asd==================----asd------")
 
 def test_product():
     assert math_functions.product(5,5) == 25
     assert math_functions.product(0,5) == 0
     assert math_functions.product(5) == 10
+
+
+def test_add_string():
+    result = math_functions.add("hello ","world")
+    assert result == "hello world"
+    assert type(result) is str
+    assert "asd" not in result
+
+
+def test_product_string():
+    result = math_functions.product("hello")
+    result2 = math_functions.product("hello " ,3)
+    assert result == "hellohello"
+    assert result2 == "hello hello hello "
